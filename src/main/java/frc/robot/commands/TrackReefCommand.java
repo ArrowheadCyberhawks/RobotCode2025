9abcafaf0149838d1ff93b506087e1c5413ed59d
Cyberhawks706 <robotics@arrowheadschools.org>
2025-01-27 20:54:11 -0600

    turn to pose kinda not working

--- src/main/java/frc/robot/commands/TrackReefCommand.java
+++ src/main/java/frc/robot/commands/TrackReefCommand.java
@@ -42,22 +42,31 @@ public class TrackReefCommand extends Command {
     @Override
     public void execute() {
         //Get real-time joystick inputs and angle from apriltag
-        Pose2d targetPose = field.getTagPose(1).orElse(new Pose3d()).toPose2d();
+        Pose2d targetPose = field.getTagPose(6).orElse(new Pose3d()).toPose2d();
         Pose2d robotPose = swerveSubsystem.swerveDrive.swerveDrivePoseEstimator.getEstimatedPosition();
         Transform2d targetPose_RobotSpace = new Transform2d(targetPose, robotPose);
-        Rotation2d targetAngle = targetPose.minus(robotPose).getTranslation().getAngle();//targetPose_RobotSpace.getRotation();
+        Rotation2d targetAngle = targetPose.minus(robotPose).getTranslation().getAngle(); //targetPose_RobotSpace.getRotation();
+
         double xSpeed = ySpdFunction.get();
         double ySpeed = xSpdFunction.get();
         double turningSpeed;
-        
+
         //Apply deadband
         xSpeed = MathUtil.applyDeadband(xSpeed, IOConstants.kDriverControllerDeadband);
         ySpeed = MathUtil.applyDeadband(ySpeed, IOConstants.kDriverControllerDeadband);
 
         //Make driving smoother
-        turningSpeed = m_turningController.calculate(targetPose_RobotSpace.getRotation().getRadians());
+        //turningSpeed = m_turningController.calculate(targetPose_RobotSpace.getRotation().getRadians());
+        turningSpeed = (targetAngle.getRadians());
+        System.out.println(turningSpeed);
+        
+
         xSpeed = Math.abs(xSpeed)*xSpeed;
         ySpeed = Math.abs(ySpeed)*ySpeed;
+
+        //TODO: GET RID OF THIS
+        xSpeed = 0;
+        ySpeed = 0;
 
         // x *= MathUtil.interpolate(0.15, 1, accelMultiplier);
 		// y *= MathUtil.interpolate(0.15, 1, accelMultiplier);
