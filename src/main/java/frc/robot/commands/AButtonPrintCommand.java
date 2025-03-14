eb51634fc8dc1c5fc2f484cbd2fbedfd95ed9ce0
cyberhawks <cyberhawks706@gmail.com>
2025-03-14 18:15:27 -0500

    wip

--- src/main/java/frc/robot/commands/AButtonPrintCommand.java
+++ src/main/java/frc/robot/commands/AButtonPrintCommand.java
@@ -5,6 +5,7 @@ import lib.frc706.cyberlib.subsystems.LimelightHelpers;
 import lib.frc706.cyberlib.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.*;
+import java.lang.Thread;
 
 //getRobotVelocity getYaw
     public class AButtonPrintCommand extends Command{
@@ -16,17 +17,52 @@ import frc.robot.subsystems.*;
             System.out.println("A Button Command Constructor");
         }
 
+    boolean loopbreak;
         
         @Override
         public void initialize(){
-        
+            
             // int[] validAprilTags = {4};
             // LimelightHelpers.SetFiducialIDFiltersOverride("limelight-three",validAprilTags);
-            
+            double tx = LimelightHelpers.getTX("limelight-three");
             boolean tv = LimelightHelpers.getTV("limelight-three");
-                //System.out.println(tv); //Temporary line for testing tv.
+               
+
+            while (tv == true && tx > 0) {
+                
+                     tx = LimelightHelpers.getTX("limelight-three");
+                     tv = LimelightHelpers.getTV("limelight-three");
+                    // if (tx > 0) {
+                    //     loopbreak = false;
+                    //     break;
+                    // }
+                        this.swerveSubsystem.driveRobotOriented(this.swerveSubsystem.swerveDrive.swerveController.getRawTargetSpeeds(
+                         0.0,-0.5,0.0));
+                        try {
+                           long sleepTime = (long)(tx+1*10);
+                           System.out.println("sleeping for " + (sleepTime));
+                        Thread.sleep(sleepTime);
+                        } catch (InterruptedException e) {
+                     e.printStackTrace();
+                    }
+                    // if (tx < 0) {
+                    //     loopbreak = true;
+                    //     break;
+                    // }
+               }
             
-            if  (tv == true) {
+                //if (loopbreak == true) {
+                    System.out.println("TX value is negative! Loop TX = "  + (tx));
+                //}
+
+
+
+            }
+                //else {
+                   // System.out.println("No valid apriltags");
+              //  }
+
+            // if  (tv == true) {
                 
                 //double aButtonGetTurnRate= this.swerveSubsystem.swerveDrive.getTurnRate();
                 //System.out.println(("RobotTurnRate = ") + (aButtonGetTurnRate));
@@ -36,17 +72,17 @@ import frc.robot.subsystems.*;
                 // System.out.println(("relativeSpeeds = ") + (aButtonRobotRelativeSpeeds));
 
 
-                double tx = LimelightHelpers.getTX("limelight-three");
-                double aButtonHeading = this.swerveSubsystem.getHeading();
-                System.out.println(("TX Value =")+(tx));
-                System.out.println(("Heading = ") + (aButtonHeading));
+                // double tx = LimelightHelpers.getTX("limelight-three");
+                // double aButtonHeading = this.swerveSubsystem.getHeading();
+                // System.out.println(("TX Value =")+(tx));
+                // System.out.println(("Heading = ") + (aButtonHeading));
                 
-            }
+        //     }
             
-            else {
-                System.out.println("No Valid AprilTags!");
-            }
-        }
+        //     else {
+        //         System.out.println("No Valid AprilTags!");
+        //     }
+         }
 
         @Override
         public void execute() {
